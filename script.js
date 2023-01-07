19e3fbcf9b7e17030203b1ab50d77d3fe05f5ea8
AnkitKhandelwal05 <107235554+AnkitKhandelwal05@users.noreply.github.com>
2023-01-07 16:16:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 var count=0;
+var b;
+var c;
+function name(){
+ b= prompt("ENTER NAME OF 1st player");
+ c= prompt("ENTER NAME OF 2nd player");
+document.getElementById(1).innerText= b.toUpperCase();
+document.getElementById(2).innerText= c.toUpperCase();
+document.getElementById("star").style.width = "0";
+}
 function cl(a){
-    
     count++;
     console.log(count)
     if (count%2==0) {
         document.getElementById(a).innerText= "X";
         document.getElementById(a).value= 1;
-    console.log("x");
+    console.log("X");
         
     } else {
         document.getElementById(a).innerText= "O";
@@ -17,7 +25,6 @@ function cl(a){
         win();
     }
     //document.getElementById("win").innerText=document.getElementById(a).value
-
 }
 function win(){
     if (
@@ -30,7 +37,7 @@ function win(){
     document.getElementById(11).value+document.getElementById(22).value+document.getElementById(33).value==0||
     document.getElementById(13).value+document.getElementById(22).value+document.getElementById(31).value==0) {
         document.getElementById("myNav").style.width = "100%";
-        document.getElementById("win").innerText="Player 1 Won"
+        document.getElementById("win").innerText= b + " Won"
     }
     else if(
     document.getElementById(21).value+document.getElementById(22).value+document.getElementById(23).value==3||
@@ -42,7 +49,14 @@ function win(){
     document.getElementById(11).value+document.getElementById(22).value+document.getElementById(33).value==3||
     document.getElementById(13).value+document.getElementById(22).value+document.getElementById(31).value==3){
         document.getElementById("myNav").style.width = "100%";
-        document.getElementById("win").innerText="Player 2 Won"
+        document.getElementById("win").innerText= c + " Won"
+    }
+    else{
+        if (count>=9) {document.getElementById("myNav").style.width = "100%"; 
+        document.getElementById("win").innerText="DRAW!!!"
+            
+        }
+        
     }
 
 }
